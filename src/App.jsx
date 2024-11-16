5521e920ffd28d76e70926b3e2045d082131fa00
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2024-11-15 23:04:48 -0500

    feat:frontend_proyecto_node

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import Navbar from "./components/navbar/Navbar"
-import Login from "./pages/Login"
+import Header from "./components/navbar/Navbar"
+
 
 function App() {
 
 
   return (
     <>
-    <Navbar />
-     <h1 className='text-center text-blue-600 text-4xl font-bold'>Bienvenido a react</h1>
-    <Login />
+    <Header />
+    
     
     </>
 
