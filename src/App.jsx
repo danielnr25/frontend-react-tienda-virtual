ac593a6d2d6e8b824b214b4b94b8d0654535e548
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2024-11-22 20:32:46 -0500

    feat:frontend_proyecto_node

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import Header from "./components/navbar/Navbar"
+import Login from "./pages/Login"
+import Home from './pages/Home'
+import About from './pages/About'
+import Collection from './pages/Collection'
 
+import { Routes,Route,Navigate,useLocation } from "react-router-dom"
 
 function App() {
 
 
   return (
     <>
-    <Header />
     
+        <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+             <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/collection" element={<Collection />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element = {<Login />} /> 
+                <Route path="*" element={<h1>PAGINA NO ENCONTRADA</h1>} />
+            </Routes>
+        </div>
     
     </>
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Home/Hero";
+
+const Home = () =>{
+    
+    return(
+        <div>
+            Desde el inicio del Home
+            <Hero />
+        
+        </div>
+    )
+}
+
+
+export default Home;
